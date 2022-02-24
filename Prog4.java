ab3e82f0061f71adeb9e58cefad36780b0c2d243
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-02-23 19:35:39 -0600

    First commit

---
+++ Prog4.java
@@ -0,0 +1,25 @@
+package w1l2.CS390_Assignments;
+
+public class Prog4 {
+
+	public static void main(String[] args) {
+		
+		float num= 1.27f, num2=	3.881f, num3 =9.6f;
+		/*
+		 * Prog4- 1;
+		 */
+		
+		int sum= (int)(num+num2+num3);
+		System.out.println(sum);
+		/*
+		 * Prog4- 2;
+		 */
+		float sumR = (num+num2+num3);
+		
+		int tem = (int)Math.round(sumR);
+		System.out.println(tem);
+		
+
+	}
+
+}
